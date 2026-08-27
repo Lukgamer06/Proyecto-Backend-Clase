6bc9674a48039b7edd11422a387653862229771a
Lucas <lukasfernando0609@gmail.com>
2026-08-27 11:28:49 -0500

    Separar Header en el Layout

--- proyecto-en-clase/resources/views/layouts/app.blade.php
+++ proyecto-en-clase/resources/views/layouts/app.blade.php
@@ -8,14 +8,8 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-
-    <header class="site-header">
-        <a href="/" class="brand">Mi E‑Commerce</a>
-        <nav class="site-nav">
-            <a href="/products">Productos</a>
-            <a href="/products/create" class="site-nav-cta">Crear producto</a>
-        </nav>
-    </header>
+    
+    @include('layouts.header')
 
     <main>
         @yield('content')
---
+++ proyecto-en-clase/resources/views/layouts/header.blade.php
@@ -0,0 +1,7 @@
+<header class="site-header">
+    <a href="/" class="brand">Mi E‑Commerce</a>
+    <nav class="site-nav">
+        <a href="/products">Productos</a>
+        <a href="/products/create" class="site-nav-cta">Crear producto</a>
+    </nav>
+</header>
