33a0a60be658c69ca557deca9a3cc62ab70cb547
Lucas Ardila <lukgamer0609@gmail.com>
2026-09-10 11:59:57 -0500

    Factory y seeders

--- proyecto-en-clase/database/seeders/DatabaseSeeder.php
+++ proyecto-en-clase/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,9 +18,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //User::factory()->create([
+        //    'name' => 'Test User',
+        //    'email' => 'test@example.com',
+        //]);
+
+
+        $category = new Category();
+        $category->name = 'Tecnologia';
+        $category->description = 'Productos tecnologicos';
+
+        $category->save();
+
+        Category::factory(1000)->create();
     }
 }
